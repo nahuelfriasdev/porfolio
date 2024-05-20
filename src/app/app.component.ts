553d8079cb12ccd29f38a61cb3039e4030c26eb8
nahuelfrias45 <nahuelfrias@outlook.com.ar>
2024-05-20 20:10:15 -0300

    cambio contenido de proyectos/trabajos y agrego footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,12 @@ import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { CommonModule } from '@angular/common';
 import { TecnologiasComponent } from './components/tecnologias/tecnologias.component';
 import { InicioComponent } from './components/inicio/inicio.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavBarComponent, PerfilComponent, ExperienciaComponent, ProyectosComponent, TecnologiasComponent, InicioComponent],
+  imports: [CommonModule, RouterOutlet, NavBarComponent, PerfilComponent, ExperienciaComponent, ProyectosComponent, TecnologiasComponent, InicioComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
